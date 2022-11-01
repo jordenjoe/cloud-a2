422e1d44d5e839844595f422f6723627ef6e6161
Jorden Jolley <94325574+jordenjoe@users.noreply.github.com>
2022-11-01 17:39:53 -0400

    audio is now working

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,17 @@
+
+
+
 $(document).ready(function() {
 
-
 });
-
 
 function callSearchApi(message) {
   console.log("inside here");
   // params, body, additionalParams
   return sdk.searchGet({q: message}, {}, {});
 }
+
+
 
 document.getElementById("btn").addEventListener("click", function () {
   var msg = document.getElementById("myInput").value;
@@ -26,3 +29,32 @@ document.getElementById("btn").addEventListener("click", function () {
   });
 
 })
+
+function record(){
+
+  var speech = true;
+    window.SpeechRecognition = window.SpeechRecognition
+                    || window.webkitSpeechRecognition;
+
+    const recognition = new SpeechRecognition();
+    recognition.interimResults = true;
+    const words = document.querySelector('.words');
+    words.appendChild(p);
+
+    recognition.addEventListener('result', e => {
+        const transcript = Array.from(e.results)
+            .map(result => result[0])
+            .map(result => result.transcript)
+            .join('')
+
+        document.getElementById("myInput").value = transcript;
+        console.log(transcript);
+    });
+     
+    
+    if (speech == true) {
+        recognition.start();
+    }
+    
+
+}

631006ab516dee9b0432fa4c7df3002918c49325
Jorden Jolley <94325574+jordenjoe@users.noreply.github.com>
2022-11-01 11:38:53 -0400

    search is mostly working, but doesn't take in two variables

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,45 +4,25 @@ $(document).ready(function() {
 });
 
 
-function callSearchApi(message, message, message) {
+function callSearchApi(message) {
   console.log("inside here");
   // params, body, additionalParams
-  return sdk.searchGet({}, {
-    messages: [{
-      type: 'unstructured',
-      unstructured: {
-        text: message
-      }
-    }]
-  }, {});
+  return sdk.searchGet({q: message}, {}, {});
 }
 
 document.getElementById("btn").addEventListener("click", function () {
   var msg = document.getElementById("myInput").value;
   console.log(msg);
 
+  displayPhotos = document.getElementById("displayPhotos");
+
   callSearchApi(msg)
   .then((response) => {
     console.log(response);
-    var data = response.data;
-    console.log(data);
-    console.log(data.body);
-
-    //if (data.body && data.body.length > 0) {
-    if (data.body) {
-      console.log('received messages');
-      
-      var messages = data.body;
-
-      insertResponseMessage(messages);
-
-    } else {
-      insertResponseMessage('Oops, something went wrong 1. Please try again.');
-    }
+    displayPhotos.innerHTML = response["data"];
   })
   .catch((error) => {
     console.log('an error occurred', error);
-    insertResponseMessage('Oops, something went wrong 2. Please try again.');
   });
 
 })
